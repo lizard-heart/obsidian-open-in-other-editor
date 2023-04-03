434abf621b6a5a97513ed85a4f00d82150472000
Henry Gustafson <hgustafson@protonmail.com>
2023-04-03 10:41:43 -0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -42,7 +42,7 @@ export default class OpenFilePlg extends Plugin {
 			return
 		}
 		let cwd = this.app.vault.adapter.getResourcePath(".")
-		cwd = cwd.replace("app://local/", "").replace(/\?\d+.*?/, "")
+		cwd = decodeURI(cwd.replace("app://local/", "").replace(/\?\d+.*?/, ""))
 		if (os.type() === "Windows_NT") {
 			runCMD(`cd /d ${cwd} && ${by} ./"${curFilePath}"`)
 		} else {
